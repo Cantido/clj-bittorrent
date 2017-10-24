96f74740e9c8d06809e48a5fc4460cf7fe1b244a
Bobby Richter <robert.c.richter@gmail.com>
2017-10-24 17:12:19 -0500

    WIP

--- src/clj_bittorrent/metainfo.clj
+++ src/clj_bittorrent/metainfo.clj
@@ -1,7 +1,10 @@
 (ns clj-bittorrent.metainfo
   (:require [clj-bencode.core :as b]
-            [clojure.set :as s])
-  (:import (org.apache.commons.io IOUtils)))
+            [clojure.set :as s]
+            [clojure.java.io :as io])
+  (:import (org.apache.commons.io IOUtils)
+           (java.security MessageDigest)
+           (java.nio.charset StandardCharsets)))
 
 (def metainfo-kmap {"announce" :announce
                     "created by" :created-by
@@ -9,14 +12,60 @@
                     "encoding" :encoding
                     "info" :info})
 
-(def piece-kmap {"length" :length
-                 "name" :name
-                 "piece length" :piece-length
-                 "pieces" :pieces
-                 "private" :private})
+(def info-kmap {"length" :length
+                "name" :name
+                "piece length" :piece-length
+                "pieces" :pieces
+                "private" :private})
+
+
+;(defn sha1 [s]
+;  (-> "sha1"
+;      MessageDigest/getInstance
+;      (.digest (bytes s))))
+
+
+;(defn hash-next-piece
+;  [piece-length s]
+;  {:pre [(< 0 piece-length)]}
+;  (let [buf (byte-array piece-length)
+;        read-count (.read (io/input-stream s) buf)]
+;    (cond
+;      (= piece-length read-count) buf
+;      (< 0 read-count piece-length) buf
+;      (<= read-count 0) nil)))
+;
+;(defn pieces
+;  [piece-length f]
+;  {:pre [(< 0 piece-length) (some? f)]}
+;  (let [stream (io/input-stream f)]
+;    (take-while some? (repeatedly #(hash-next-piece piece-length f)))))
+
+(def sha1-byte-count 20)
+
+(defn expected-piece-count [m]
+  (let [{:keys [info]} m]
+    (let [{:keys [length piece-length]} info]
+      (int (Math/ceil (float (/ length piece-length)))))))
+
+(defn sha1-seq
+  "Decodes a UTF-16 string into a seq of 20-byte SHA-1 byte-arrays."
+  [piece-length coll]
+  {:pre [(< 0 piece-length)]}
+  (seq (map byte-array (partition sha1-byte-count (b/to-utf8 coll)))))
+
+(defn encode-pieces
+  "Updates the :pieces element in the array to be 20-byte byte
+   arrays."
+  [x]
+  (let [piece-length (get-in x [:info :piece-length])]
+    (update-in x [:info :pieces] (partial sha1-seq piece-length))))
 
 (defn read-metainfo [x]
   (-> x
-    (b/decode)
-    (s/rename-keys metainfo-kmap)
-    (update-in [:info] #(s/rename-keys % piece-kmap))))
+      (b/decode)
+      (s/rename-keys metainfo-kmap)
+      (update-in [:info] #(s/rename-keys % info-kmap))
+      (update-in [:info :pieces] #(.getBytes (str %) StandardCharsets/US_ASCII))))
+    ;(encode-pieces)))
+
--- test/clj_bittorrent/metainfo_test.clj
+++ test/clj_bittorrent/metainfo_test.clj
@@ -4,11 +4,18 @@
             [clj-bittorrent.metainfo :as m])
   (:import (java.io File)))
 
-(def file ^File
+(def metainfo-file ^File
   (io/file (io/resource "linuxmint-18.2-cinnamon-64bit.iso.torrent")))
 
+(def iso-file ^File
+  (io/file (io/resource "linuxmint-18.2-cinnamon-64bit.iso")))
+
+(defn hex [n]
+  (format "%02x" n));
+
 (deftest read-file
-  (let [result (m/read-metainfo file)]
+  (let [result (m/read-metainfo metainfo-file)]
+    (is (= 1599 (m/expected-piece-count result)))
     (is (= "https://torrents.linuxmint.com/announce.php" (:announce result)))
     (is (= "Transmission/2.84 (14307)" (:created-by result)))
     (is (= 1499021259 (:creation-date result)))
@@ -18,5 +25,8 @@
       (is (= 1676083200 (:length info)))
       (is (= "linuxmint-18.2-cinnamon-64bit.iso" (:name info)))
       (is (= 1048576 (:piece-length info)))
-      (is (not= nil (:pieces info)))
-      (is (= 0 (:private info))))))
+      (is (= 0 (:private info)))
+      ;(is (= true (every? #(= 20 (count %)) (:pieces info))))
+      ;(is (= "ef" (hex (ffirst (:pieces info)))))
+      (is (= (* 20 1599) (count (partition 20 (:pieces info))))))))
+
