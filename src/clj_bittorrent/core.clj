7cd59be4c0537ed0dd8a546958536f4395f62808
Bobby Richter <robert.c.richter@gmail.com>
2017-10-24 00:05:37 -0500

    Remove the default "core" namespace
    What I currently have should not yet be considered "core"

--- src/clj_bittorrent/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-bittorrent.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/clj_bittorrent/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-bittorrent.core-test
-  (:require [clojure.test :refer :all]
-            [clj-bittorrent.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
