2e5c7cf8797d40a3c4ff4f8346fa6ca3c8a45d16
Bobby Richter <robert.c.richter@gmail.com>
2017-10-29 13:47:42 -0500

    Add type hints
    I like type hints

--- src/clj_bittorrent/binary.clj
+++ src/clj_bittorrent/binary.clj
@@ -1,33 +1,36 @@
 (ns clj-bittorrent.binary
   "Byte-manipulation functions.")
 
-(defn ubyte? [x] (<= 0 x 255))
-(defn sbyte? [x] (<= -128 x 127))
+(defn ubyte? ^Boolean [x] (<= 0 (int x) 255))
+(defn sbyte? ^Boolean [x] (<= -128 (int x) 127))
 
 (defn ubyte
   "converts a signed byte to an unsigned byte.
    Unsigned bytes must be stored as integers."
-  [b]
+  ^Integer [b]
   {:pre [(sbyte? b)]
    :post [(ubyte? %)]}
-  (Byte/toUnsignedInt b))
+  (Byte/toUnsignedInt (byte b)))
 
 (defn sbyte
   "Converts an unsigned byte into a signed byte."
-  [b]
+  ^Byte [b]
   {:pre [(ubyte? b)]
    :post [(sbyte? %)]}
   (unchecked-byte (+ 256 b)))
 
-(defn rand-ubyte []
+(defn rand-ubyte
+  ^Integer []
   {:post [(ubyte? %)]}
   (rand-int 256))
 
-(defn rand-sbyte []
+(defn rand-sbyte
+  ^Byte []
   {:post [(sbyte? %)]}
   (sbyte (rand-ubyte)))
 
-(defn hexbyte [b]
+(defn hexbyte
+  ^String [b]
   {:pre [(ubyte? b)]
    :post [(= 2 (count (seq %)))]}
-  (format "%02X" b))
+  (format "%02X" (int b)))
