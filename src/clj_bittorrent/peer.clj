9cfad21da3e61a87910abefdcc725b70c9884552
Bobby Richter <robert.c.richter@gmail.com>
2017-11-05 17:32:59 -0600

    Add schema validation to Peer functions

--- src/clj_bittorrent/peer.clj
+++ src/clj_bittorrent/peer.clj
@@ -39,72 +39,70 @@
    :blocks     HasBlocks
    :requested  RequestedBlocks})
 
-(def peer-default-state
+(schema/def peer-default-state :- Peer
   {:choked true
    :interested false
    :have #{}
    :blocks #{}
    :requested #{}})
 
-(defn choke
+(schema/defn choke :- Peer
   "Choke the peer. The peer that is choked will be ignored until it is unchoked."
-  [peer]
+  [peer :- Peer]
   (assoc peer :choked true))
 
-(defn unchoke
+(schema/defn unchoke :- Peer
   "Unchoke the peer. The peer will no longer be ignored."
-  [peer]
+  [peer :- Peer]
   (assoc peer :choked false))
 
-(defn interested
+(schema/defn interested :- Peer
   "Mark the peer as interested. An interested peer wants something that
    other peers have to offer, and will begin requesting blocks."
-  [peer]
+  [peer :- Peer]
   (assoc peer :interested true))
 
-(defn not-interested
+(schema/defn not-interested :- Peer
   "Mark the peer as not interested. A peer that is not interested will not
    send requests for data to other peers."
-  [peer]
+  [peer :- Peer]
   (assoc peer :interested false))
 
-(defn- index? [x]
-  (and (number? x)
-       (not (neg? x))))
-
-(defn- un-request-blocks
-  ([peer index-or-block]
+(schema/defn un-request-blocks :- Peer
+  ([peer :- Peer
+    index-or-block]
    (let [index (if (number? index-or-block)
                  index-or-block
                  (:index index-or-block))]
      (update peer :requested #(blocks/remove-blocks-matching-indices % index)))))
 
-(defn has-piece
+(schema/defn has-piece :- Peer
   "Update the peer to say they have piece n. Also clears that piece from
    the set of requested blocks, since the peer has it already."
-  ([peer n]
-   {:pre [(index? n)]
-    :post [(contains? (:have %) n)]}
+  ([peer :- Peer
+    n :- n/Index]
    (-> peer
        (update :have #(conj (set %) n))
        (un-request-blocks n)))
   ([peer n & ns]
    (reduce has-piece (has-piece peer n) ns)))
 
-(defn request-block
+(schema/defn request-block :- Peer
   "Add a request for a block to a peer. If the peer has that block in their
    \"have\" set, then that block is also removed from that set."
-  [peer block]
+  [peer :- Peer
+   block :- blocks/BlockId]
   {:post [(not (contains? (:have %) (:index block)))
           (contains? (:requested %) block)]}
   (-> peer
       (update :have #(disj (set %) (:index block)))
       (update :requested #(conj (set %) block))))
 
-(defn add-block
+(schema/defn add-block :- Peer
   "Add a block of data to a peer. If the peer requested the given block, that
    block will be removed from the requested set as well."
-  [peer block]
+  [peer :- Peer
+   block :- blocks/BlockData]
   (-> peer
       (update :blocks #(blocks/conj-condense (set %)
                                              (select-keys block [:index :offset :contents])))
--- test/clj_bittorrent/peer_test.clj
+++ test/clj_bittorrent/peer_test.clj
@@ -1,33 +1,46 @@
 (ns clj-bittorrent.peer-test
   (:require [clojure.test :refer :all]
-            [clj-bittorrent.peer :as peer]))
+            [clj-bittorrent.peer :as peer]
+            [schema.test :as st]))
+
+(use-fixtures :once st/validate-schemas)
+
+(defn peer-with [key val]
+  (assoc peer/peer-default-state key val))
+
+(defn block-id [i]
+  {:index i :offset 1 :length 1})
 
 (deftest choke-test
-  (is (= true (:choked (peer/choke {:choked false}))))
-  (is (= true (:choked (peer/choke {:choked true}))))
-  (is (= true (:choked (peer/choke {})))))
+  (is (= true (:choked (peer/choke (peer-with :choked false)))))
+  (is (= true (:choked (peer/choke (peer-with :choked true))))))
 
 (deftest unchoke-test
-  (is (= false (:choked (peer/unchoke {:choked false}))))
-  (is (= false (:choked (peer/unchoke {:choked true}))))
-  (is (= false (:choked (peer/unchoke {})))))
+  (is (= false (:choked (peer/unchoke (peer-with :choked false)))))
+  (is (= false (:choked (peer/unchoke (peer-with :choked true))))))
 
 (deftest interested-test
-  (is (= true (:interested (peer/interested {:interested false}))))
-  (is (= true (:interested (peer/interested {:interested true}))))
-  (is (= true (:interested (peer/interested {})))))
+  (is (= true (:interested (peer/interested (peer-with :interested false)))))
+  (is (= true (:interested (peer/interested (peer-with :interested true))))))
 
 (deftest not-interested-test
-  (is (= false (:interested (peer/not-interested {:interested false}))))
-  (is (= false (:interested (peer/not-interested {:interested true}))))
-  (is (= false (:interested (peer/not-interested {})))))
+  (is (= false (:interested (peer/not-interested (peer-with :interested false)))))
+  (is (= false (:interested (peer/not-interested (peer-with :interested true))))))
 
 (deftest has-piece-test
-  (is (= #{666} (:have (peer/has-piece {:have #{}} 666))))
-  (let [result (peer/has-piece {:requested #{{:index 420} {:index 666}}} 666)]
+  (is (= #{666} (:have (peer/has-piece (peer-with :have #{}) 666))))
+
+  (let [result (peer/has-piece
+                 (peer-with :requested #{(block-id 420) (block-id 666)})
+                 666)]
+
     (is (= #{666} (:have result)))
-    (is (= #{{:index 420}} (:requested result))))
-  (let [result (peer/has-piece {:requested #{{:index 420} {:index 666}}} 666 420)]
+    (is (= #{(block-id 420)} (:requested result))))
+
+  (let [result (peer/has-piece
+                 (peer-with :requested #{(block-id 420) (block-id 666)})
+                 666 420)]
+
     (is (= #{666 420} (:have result)))
     (is (= #{} (:requested result)))))
 
@@ -42,19 +55,23 @@
   (testing "adding a block to a peer"
     (is (= #{piece-zero-one}
            (:blocks (peer/add-block
-                      {:blocks #{piece-zero}}
+                      (peer-with :blocks #{piece-zero})
                       piece-one))))
     (testing "when pieces in the same block aren't adjacent"
       (is (= #{piece-zero piece-two}
              (:blocks (peer/add-block
-                        {:blocks #{piece-zero}}
+                        (peer-with :blocks #{piece-zero})
                         piece-two)))))
     (testing "when a third block becomes adjacent between two existing blocks"))
   (is (= #{piece-all}
          (:blocks (peer/add-block
-                    {:blocks #{piece-zero piece-two}}
+                    (peer-with :blocks #{piece-zero piece-two})
                     piece-one)))))
 
 (deftest request-test
-  (is (= #{{:index 0 :offset 0 :length 1}} (:requested (peer/request-block {} {:index 0 :offset 0 :length 1}))))
-  (is (= #{{:index 111 :offset 222 :length 333}} (:requested (peer/request-block {} {:index 111 :offset 222 :length 333})))))
+  (is (= #{{:index 0 :offset 0 :length 1}}
+         (:requested (peer/request-block peer/peer-default-state
+                                         {:index 0 :offset 0 :length 1}))))
+  (is (= #{{:index 111 :offset 222 :length 333}}
+         (:requested (peer/request-block peer/peer-default-state
+                                         {:index 111 :offset 222 :length 333})))))
