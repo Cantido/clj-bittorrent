b717d5fcabcb9e14b0d74a024dce06a2d702d29c
Bobby Richter <robert.c.richter@gmail.com>
2017-11-09 19:55:45 -0600

    Add validation of piece sets

--- src/clj_bittorrent/pieces/pieces.clj
+++ src/clj_bittorrent/pieces/pieces.clj
@@ -5,9 +5,24 @@
             [clojure.set :as s]))
 
 (defn valid-piece? [m p]
-  (let [actual-sha (hash/sha1 (:contents p))
-        expected-sha (get (:pieces m) (:index p))]
+  (let [actual-sha (seq (hash/sha1 (byte-array (:contents p))))
+        expected-sha (seq (byte-array (get (:pieces m) (:index p))))]
     (= expected-sha actual-sha)))
+
+(defn valid-pieces [m p]
+  (s/select
+    (partial valid-piece? m)
+    (set p)))
+
+(defn invalid-pieces [m p]
+  (s/difference
+    (set p)
+    (valid-pieces m p)))
+
+(defn validate [m p]
+  (let [valid (valid-pieces m p)
+        invalid (s/difference (set p) valid)]
+    [valid invalid]))
 
 (defn has-index? [n m]
   (= n (:index m)))
--- test/clj_bittorrent/pieces/pieces_test.clj
+++ test/clj_bittorrent/pieces/pieces_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [clj-bittorrent.pieces.pieces :as pieces]))
 
+(def info [1 2 3])
+
+(def info-hash
+  (byte-array
+    [112 55 -128 113 -104
+     -62 42 125 43 8
+     7 55 29 118 55
+     121 -88 79 -33 -49]))
+
+(def piece-zero-good
+  {:index 0
+   :contents info})
+
+(def piece-zero-bad
+  {:index 0
+   :contents [4 5 6]})
+
+(def metainfo
+  {:pieces [info-hash]})
+
 (deftest has-index-test
   (is (= true
          (pieces/has-index? 5
@@ -31,3 +51,71 @@
          (pieces/remove-indexed 1
            #{{:index 0}}))))
 
+(deftest valid-piece?-test
+  (is (= true
+         (pieces/valid-piece?
+           {:pieces [info-hash]}
+           {:index 0
+            :contents info})))
+  (is (= false
+         (pieces/valid-piece?
+           {:pieces [info-hash]}
+           {:index 0
+            :contents [4 5 6]})))
+  (is (= false
+         (pieces/valid-piece?
+           {:pieces [5 6 7]}
+           {:index 0
+            :contents info}))))
+
+(deftest valid-pieces-test
+  (is (= #{piece-zero-good}
+         (pieces/valid-pieces
+           metainfo
+           #{piece-zero-good})))
+
+  (is (= #{}
+         (pieces/valid-pieces
+           metainfo
+           #{piece-zero-bad})))
+
+  (is (= #{}
+         (pieces/valid-pieces metainfo #{}))))
+
+(deftest invalid-pieces-test
+  (is (= #{}
+         (pieces/invalid-pieces
+           metainfo
+           #{piece-zero-good})))
+
+  (is (= #{piece-zero-bad}
+         (pieces/invalid-pieces
+           metainfo
+           #{piece-zero-bad})))
+  (is (= #{}
+         (pieces/invalid-pieces
+           {:pieces [info-hash]}
+           #{}))))
+
+(deftest validate-test
+  (is (= [#{} #{}]
+         (pieces/validate metainfo #{})))
+
+  (is (= [#{piece-zero-good}
+          #{}]
+         (pieces/validate
+           metainfo
+           #{piece-zero-good})))
+
+  (is (= [#{}
+          #{piece-zero-bad}]
+         (pieces/validate
+           metainfo
+           #{piece-zero-bad})))
+
+  (is (= [#{piece-zero-good}
+          #{piece-zero-bad}]
+         (pieces/validate
+           metainfo
+           #{piece-zero-good
+             piece-zero-bad}))))
