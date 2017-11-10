35b099dfaed60336d216d247349ab66c30357ad7
Bobby Richter <robert.c.richter@gmail.com>
2017-11-10 12:50:01 -0600

    Use my fork of mmap

--- src/clj_bittorrent/pieces/io.clj
+++ src/clj_bittorrent/pieces/io.clj
@@ -1,13 +1,12 @@
 (ns clj-bittorrent.pieces.io
-  (:require [clj-mmap :as mmap]))
+  (:require [clj-mmap.core :as mmap]))
 
 (defn write-pieces
   "Write all data possessed by the client for torrent m to the filename at f."
-  [m client f]
+  [m ps f]
   (with-open [mapped-file (mmap/get-mmap f)]
-    (for [p (:pieces client)
-          :let [{:keys [index offset contents]} p
-                {:keys [piece-length]} m
-                piece-pos (* index piece-length)]
-          :when (= (count contents) piece-length)]
+    (for [p ps
+          :let [{:keys [index contents]} p
+                piece-length (count contents)
+                piece-pos (* index piece-length)]]
       (mmap/put-bytes mapped-file contents piece-pos))))
