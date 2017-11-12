0a8352340d9575762746feeaeb3c53a75e5fc231
Bobby Richter <robert.c.richter@gmail.com>
2017-11-12 16:03:09 -0600

    Split test dependencies, test more clojures

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "https://github.com/Cantido/clj-bittorrent"
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.txt"}
-  :profiles {:dev {:resource-paths ["test-resources"]}}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/test.check "0.9.0"]
-                 [clj-bencode "4.0.0"]
+  :profiles {:dev {:resource-paths ["test-resources"]
+                   :dependencies [[org.clojure/test.check "0.9.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-beta4"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+  :dependencies [[clj-bencode "4.0.1"]
                  [clj-http "3.7.0"]
                  [org.clojars.cantido/clj-mmap "2.1.0"]
                  [prismatic/schema "1.1.7"]])
