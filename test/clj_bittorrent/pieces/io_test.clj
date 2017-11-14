048f3998d2f77e5ab0f1038221f913183f5f6d3a
Bobbi Richter <bobby.richter@asynchrony.com>
2017-11-14 12:22:01 -0600

    Clean up write-pieces test

--- test/clj_bittorrent/pieces/io_test.clj
+++ test/clj_bittorrent/pieces/io_test.clj
@@ -4,16 +4,18 @@
             [clj-bittorrent.pieces.io :as pio])
   (:import (java.io Reader)))
 
+(def test-file (io/resource "testfile.bin"))
+
+(use-fixtures :each
+              (fn [f]
+                (spit test-file "")
+                (f)
+                (spit test-file "")))
+
+(def abc-meta {:piece-length 3 :pieces [1]})
+
+(def abc-piece {:index 0 :contents [97 98 99]})
+
 (deftest write-pieces-test
-  (let [test-file (io/resource "testfile.bin")]
-    (is (= nil
-           (pio/write-pieces
-             {:piece-length 3
-              :pieces [1]}
-             #{{:index 0 :contents [97 98 99]}}
-             test-file)))
-    (spit test-file "")))
-
-  ;(is (= "abc" (slurp (io/resource "testfile.bin")))))
-
-
+  (pio/write-pieces abc-meta #{abc-piece} test-file)
+  (is (= "abc" (slurp test-file))))
