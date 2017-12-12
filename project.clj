5a6c03c87de27539cb61febc2a93c8d988694438
Rosa Richter <robert.c.richter@gmail.com>
2017-12-12 16:01:11 -0600

    Upgrade clojure 1.9 profile to use release

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.gnu.org/licenses/gpl.txt"}
   :profiles {:dev {:resource-paths ["test-resources"]
                    :dependencies [[org.clojure/test.check "0.9.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-beta4"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :dependencies [[clj-bencode "4.0.1"]
